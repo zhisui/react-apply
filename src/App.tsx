4a855097121dd8ce636a2511b95b3bf03598adc1
zhisui <1626676263@qq.com>
2023-12-26 17:39:26 +0800

    feat:init

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function App() {
+    return <div>根组件修改哈是</div>
+}
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = {
+  // ...其他配置
+  devServer: {
+    port: 8888, // 指定端口号
+  },
+};
