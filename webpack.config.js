e2a653be52b0503f38f8e8a09825e33b3ae15e24
zhisui <1626676263@qq.com>
2023-12-27 15:25:05 +0800

    feat:update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  // ...其他配置
+  entry: './'
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
   devServer: {
     port: 8888, // 指定端口号
   },
